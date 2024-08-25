a34b98dd25bb63348561f0ea68ef5b15ce596845
Fernando Rivera <riveraf30@gmail.com>
2024-08-24 23:01:23 -0400

    Update config-client.ts

--- config-client.ts
+++ config-client.ts
@@ -1,9 +1,9 @@
 const client = {
-  appName: fillDefault(process.env.appName, "OhMyGPT"),
-  appLogo: process.env.appLogo ?? undefined,
+  appName: fillDefault(process.env.appName, "QuadMark"),
+  appLogo: process.env.appLogo, "Alex Agent.png",
   appThemeColor: fillDefault(process.env.appThemeColor, "#22c55e"),
-  appSummary: fillDefault(process.env.appSummary, "Ask me any thing you want."),
-  exampleInput: process.env.exampleInput ?? "Ask me any thing.",
+  appSummary: fillDefault(process.env.appSummary,"Streamline agent management with our powerful app. Manage marketing tasks, lead generation, and API connections efficiently. Optimize workflows, monitor performance, and enhance productivity across all channels. Designed for agent supervisors, this app simplifies the complexities of agent management, enabling you to focus on growth."),
+  exampleInput: process.env.exampleInput, "Manage lead generation and marketing tasks effectively with our app. Optimize workflows, handle API connections, and monitor agent performance to boost productivity.",
 };
 
 export default client;
