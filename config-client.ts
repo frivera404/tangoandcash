d0b08e22c9bead17fd8ab955c92adc0f33d9de86
Fernando Rivera <riveraf30@gmail.com>
2024-09-05 22:11:35 -0400

    Update config-client.ts

--- config-client.ts
+++ config-client.ts
@@ -1,6 +1,6 @@
 const client = {
   appName: fillDefault(process.env.appName, "Alex Agent"),
-  appLogo: (process.env.appLogo, "Alex Agent.png"):,
+  appLogo: (process.env.appLogo, "Alex Agent.png"):
   appThemeColor: fillDefault(process.env.appThemeColor, "#22c55e"),
   appSummary: fillDefault(process.env.appSummary,"Streamline agent management with our powerful app. Manage marketing tasks, lead generation, and API connections efficiently. Optimize workflows, monitor performance, and enhance productivity across all channels. Designed for agent supervisors, this app simplifies the complexities of agent management, enabling you to focus on growth."),
   exampleInput: process.env.exampleInput, "Manage lead generation and marketing tasks effectively with our app. Optimize workflows, handle API connections, and monitor agent performance to boost productivity.",
