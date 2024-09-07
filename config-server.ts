326d5c8b85df26fad9484470e74b0871fc8b142d
Fernando Rivera <riveraf30@gmail.com>
2024-09-06 22:58:49 -0400

    Update config-server.ts

--- config-server.ts
+++ config-server.ts
@@ -2,7 +2,7 @@ const server = {
   openAIAPIKey: required(process.env.OPENAI_API_KEY, "sk-U7KqWJD_hRgfobyuzRCBhClONupWt27T_A5l1A-RtaT3BlbkFJyiet7i8KrDtErujKiu_qGd9LyO9VuJX81QEtfpBCwA"),
   openAIAPIBaseURL: fillDefault(
     process.env.OPENAI_API_BASE_URL,
-   "https://api.openai.com/v1/chat/completions"
+   "https://chatgpt.com/g/g-gqVbcFKSK-tiktook-automation"
   ),
   systemMessage: optional(process.env.SYSTEM_MESSAGE),
   messageTemplate: optional(process.env.MESSAGE_TEMPLATE),
